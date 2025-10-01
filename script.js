7d8f257590a833b9fbd69bbe99b848ba4581dd49
526cloiacono <526cloiacono@frhsd.com>
2025-10-01 09:14:05 -0400

    started

---
+++ script.js
@@ -0,0 +1,16 @@
+const foods =[
+    "Pasta",
+    "Steak",
+    "Chicken",
+    "Chips",
+    "Pizza",
+    "Lobster",
+    "Beef",
+    "Tacos",
+    "Sushi",
+    "Wings"
+];
+
+array.forEach(foods => {
+    document.getElementById('output')
+});
